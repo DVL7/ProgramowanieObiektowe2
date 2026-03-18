72504712b2ce30fa87751893499c37e177a34bb6
Dawid Deryło <116925734+DVL7@users.noreply.github.com>
2026-03-18 18:24:20 +0100

    Zad1 działa - dodać komentarze

---
+++ lab1/TrojmianKwadratowy/Program.cs
@@ -0,0 +1,59 @@
+﻿// Program.cs 
+
+
+// deklaracja zmiennych
+// zmienne wejściowe
+double a;
+double b;
+double c;
+
+// wczytywanie zmiennej a 
+Console.WriteLine("Podaj wartość zmiennej a (różny od 0):  ");
+// walidacja sprawdzająca czy a może być przekonwertowane do formatu double oraz a != 0
+while (!double.TryParse(Console.ReadLine(), out a) || a == 0)
+{
+    Console.WriteLine("Błąd! Spróbuj jeszcze raz.");
+}
+
+// wczytywanie zmiennej b
+Console.WriteLine("Podaj wartość zmiennej b: ");
+// walidacja sprawdzająca czy b może być przekonwertowane do formatu double
+while (!double.TryParse(Console.ReadLine(), out b))
+{
+    Console.WriteLine("Błąd! Spróbuj jeszcze raz.");
+}
+
+// wczytywanie zmiennej c
+Console.WriteLine("Podaj wartość zmiennej c: ");
+// walidacja sprawdzająca czy c może być przekonwertowane do formatu double
+while (!double.TryParse(Console.ReadLine(), out c))
+{
+    Console.WriteLine("Błąd! Spróbuj jeszcze raz.");
+}
+
+// tworzenie obiektu klasy TrojmianKwadratowy
+TrojmianKwadratowy funkcja = new TrojmianKwadratowy(a, b, c);
+
+Console.WriteLine($"\nTrójmian: {a}^x2 + {b}x + c\n");
+Console.WriteLine($"Delta = {funkcja.Delta}\n");
+
+if (funkcja.Delta > 0)
+{
+    Console.WriteLine("Rozwiązania:\n");
+    Console.WriteLine($"X1 = {funkcja.X1}");
+    Console.WriteLine($"X2 = {funkcja.X2}");
+}
+else if (funkcja.Delta == 0)
+{
+    Console.WriteLine("Rozwiązanie:\n");
+    Console.WriteLine($"X1 = {funkcja.X1}");
+}
+else 
+{ 
+    Console.WriteLine("Brak rozwiązań"); 
+}
+
+
+// Console.WriteLine($"f(x) =  {a}x^2 + {b}x + {c}");
+
+
---
+++ lab1/TrojmianKwadratowy/TrojmianKwadratowy.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+/// <summary>
+/// Klasa służąca do obliczania trójmianu kwadratowego. 
+/// </summary>
+public class TrojmianKwadratowy
+{
+
+	private double a;
+    private double b;
+    private double c;
+
+    // Właściwości (gettery) dla rozwiązań. Używamy double?, bo mogą nie istnieć.
+    public double? X1 { get; private set; }
+    public double? X2 { get; private set; }
+    public double Delta { get; private set; }
+
+    public TrojmianKwadratowy(double a, double b, double c)
+	{
+		this.a = a;
+		this.b = b;
+		this.c = c;
+
+		ObliczDelte();
+		WyznaczRozwiazania();
+	}
+
+	private void ObliczDelte()
+	{
+		// Oblicz wartość delty (double) 
+		Delta =  (b*b - (4 * a * c));
+	}
+
+	private void WyznaczRozwiazania()
+	{
+
+        // delta > 0 - dwa rozwiązania
+        if (Delta > 0)
+		{
+			// rozwiązanie 1 
+			X1 = (-b + Math.Sqrt(Delta)) / (2 * a);
+			// rozwiązanie 2
+			X2 = (-b - Math.Sqrt(Delta)) / (2 * a);
+
+		}
+		// delta == 0 - jedno rozwiązanie
+		else if (Delta == 0)
+		{
+			X1 = -b / (2 * a);
+		}
+		// Delta ujemna - brak rozwiązań
+		else { }
+	}
+}
