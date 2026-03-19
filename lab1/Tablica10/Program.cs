86d858686193b5cce8f8e8e15447b64def8a798b
Dawid Deryło <116925734+DVL7@users.noreply.github.com>
2026-03-19 09:22:19 +0100

    Zad2 działa

---
+++ lab1/Tablica10/Program.cs
@@ -0,0 +1,28 @@
+﻿using Tablica10;
+
+double[] tab = new double[10];
+
+for (int i = 0; i < tab.Length; i++)
+{
+    Console.WriteLine($"Podaj liczbę {i}");
+    while (!Double.TryParse(Console.ReadLine(), out tab[i]))
+    {
+        Console.Clear();
+        Console.WriteLine("Podana wartość jest nieprawidłowa! Spróbuj jeszcze raz.");
+        Console.WriteLine($"Podaj liczbę {i}");
+
+    }
+
+}
+
+Tablica tablica = new Tablica(tab);
+
+Console.Clear();
+tablica.WyswietlTablice();
+Console.WriteLine("\n");
+
+Console.WriteLine("Suma: " + tablica.Suma);
+Console.WriteLine("Srednia: " + tablica.Srednia);
+Console.WriteLine("Min: " + tablica.Min);
+Console.WriteLine("Max: " + tablica.Max);
+
---
+++ lab1/Tablica10/Tablica.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tablica10
+{
+    internal class Tablica
+    {
+        private double[] tablica;
+
+        public double Suma { get; private set; }
+        public double Iloczyn { get; private set; }
+        public double Srednia { get; private set; }
+        public double Min { get; private set; }
+        public double Max { get; private set; }
+
+        public Tablica(double[] tablica)
+        {
+            this.tablica = tablica;
+
+            ObliczSume();
+            ObliczIloczyn();
+            ObliczSrednia();
+            ObliczMin();
+            ObliczMax();
+        }
+
+        public void WyswietlTablice()
+        {
+
+            Console.WriteLine($"[{tablica[0]}][{tablica[1]}][{tablica[2]}][{tablica[3]}][{tablica[4]}][{tablica[5]}][{tablica[6]}][{tablica[7]}][{tablica[8]}][{tablica[9]}]");
+
+        }
+
+        private void ObliczSume()
+        {
+            for (int i = 0; i < tablica.Length; i++)
+            {
+                Suma += tablica[i];
+            }
+        }
+
+        private void ObliczIloczyn()
+        {
+            for (int i = 0; i < tablica.Length; i++)
+            {
+                Iloczyn *= tablica[i];
+            }
+        }
+
+        private void ObliczSrednia()
+        {
+            Srednia = Suma / tablica.Length;
+        }
+
+        private void ObliczMin()
+        {
+            for (int i = 0;i < tablica.Length;i++)
+            {
+
+                if (i == 0)
+                {
+                    Min = tablica[i];
+                }
+
+                if (tablica[i] < Min)
+                {
+                    Min = tablica[i];
+                }
+            }
+        }
+
+        private void ObliczMax()
+        {
+            for (int i = 0; i < tablica.Length; i++)
+            {
+                if (i == 0)
+                {
+                    Max = tablica[i];
+                }
+
+                if (tablica[i] > Max)
+                {
+                    Max = tablica[i];
+                }
+            }
+        }
+
+
+    }
+}
