749590db6f866ff4606aec6e61e6261b9f58fc87
DVL7 <derylodawid@gmail.com>
2026-04-07 01:43:59 +0200

    lab1 - done

---
+++ lab1/SortowanieLiczb/Program.cs
@@ -0,0 +1,24 @@
+﻿using SortowanieLiczb;
+
+Sortowanie sort = new Sortowanie();
+double liczba;
+
+while(true)
+{
+    Console.Clear();
+
+    sort.WyswietlTablice();
+    Console.WriteLine("\nPodaj liczbę: ");
+
+    while (!Double.TryParse(Console.ReadLine(), out liczba))
+    {
+        Console.Clear();
+        sort.WyswietlTablice();
+
+        Console.WriteLine("\nPodana wartość jest nieprawidłowa! Spróbuj jeszcze raz.");
+        Console.WriteLine("Podaj liczbę: ");
+    }
+
+    sort.DodajDoListy(liczba);
+    sort.Posortuj();
+}
