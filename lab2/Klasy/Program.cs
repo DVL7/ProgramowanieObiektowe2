752eb1ecc375e49ff7f06aced03e0859d3351f6a
DVL7 <derylodawid@gmail.com>
2026-04-08 23:54:34 +0200

    zadanie 2.1 Osoba

---
+++ lab2/Klasy/Program.cs
@@ -0,0 +1,15 @@
+﻿using Klasy;
+
+Console.WriteLine($"Podaj imie: ");
+string? imie = Console.ReadLine();
+
+Console.WriteLine($"Podaj nazwisko: ");
+string? nazwisko = Console.ReadLine();
+
+Console.WriteLine($"Podaj wiek: ");
+string? wiek = Console.ReadLine();
+
+Osoba osoba = new Osoba(imie, nazwisko, wiek);
+
+Console.WriteLine("\n\n");
+osoba.WyswietlInformacje();
